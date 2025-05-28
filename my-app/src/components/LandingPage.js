642fe9ba7fecc1a44b7a436220ed0f9cd1ed26a7
1eehvunzin <hyunjinlee@ewha.ac.kr>
2025-05-28 22:47:37 +0900

    디자인 및 문항 수정 반영

--- my-app/src/components/LandingPage.js
+++ my-app/src/components/LandingPage.js
@@ -8,18 +8,35 @@ function LandingPage({ onMenuClick}) {
 
   return (
     <div style={{
-      width: '50vw',
+      width: '100vw',
+      height: '100vh',
       maxWidth: '600px',
       minWidth: '300px',
       margin: '0 auto',
-      padding: '2rem'
+      padding: '0',
+      background: '#e3eeb4',
+
+      display: 'flex',           // flex 컨테이너로 만들기
+      flexDirection: 'column',   // 세로 방향 정렬
+      alignItems: 'center',      // 하위 요소 가로 중앙정렬
     }}>
       {/* 가운데 - 제목 */}
-      <h1 style={{textAlign: 'center'}}>LandingPage</h1>
-
-      <div style={{display: 'flex', justifyContent: 'center'}}>
-        <button className="button" onClick={() => navigate('/page1')}>시작하기</button>
+      <div className='top-bar'>
+        <h1 style={{textAlign: 'center', margin: 0}}>D I C E</h1>
       </div>
+      <img src="/images/landing.png" alt="landing" style={{ width: '244px', height: '244px', margin: '25px'}}/>
+      <h2 style={{width: '90%', textAlign:'left'}}>데이터로 예측하고,<br/>디지털로 경고하다.</h2>
+      <h4 style={{width: '90%', textAlign:'left'}}>
+        🌿 치매는 하루아침에 오지 않습니다.<br/>
+        📉 하지만 기억력, 감정, 수면, 습관…<br/>
+        📍 일상의 아주 작은 변화들이 축적될 수 있습니다.<br/>
+        📊 우리는 그 변화를 숫자로 감지하고,<br/>
+        📥 의미 있는 조언으로 되돌려 드립니다.<br/>
+        🔔 지금의 신호를, 놓치지 않기 위해.<br/>
+        🧠 그래서 우리는, DICE를 만들었습니다. </h4>
+
+      <button className="button" onClick={() => navigate('/page1')}>시작하기</button>
+
     </div>
   );
 }
--- my-app/src/components/Page1.js
+++ my-app/src/components/Page1.js
@@ -11,17 +11,25 @@ function Page1({ onMenuClick}) {
 
     return (
     <div style={{
-      width: '50vw',
+      width: '100vw',
+      height: '100vh',
       maxWidth: '600px',
       minWidth: '300px',
       margin: '0 auto',
-      padding: '2rem'
+      padding: '0',
+      // background: '#e3eeb4',
+
+      display: 'flex',           // flex 컨테이너로 만들기
+      flexDirection: 'column',   // 세로 방향 정렬
+      alignItems: 'center',      // 하위 요소 가로 중앙정렬
     }}>
     
     {/* 가운데 - 제목 */}
-    <h1 style={{textAlign: 'center'}}>[인지기능 영역]</h1>
+    <div className='top-bar'>
+        <h1 style={{textAlign: 'center', margin: 0}}>[인지기능 영역]</h1>
+    </div>
 
-    <h2 style={{textAlign: 'left', marginTop: '50px'}}>1. 오늘 날짜를 선택해주세요.</h2>
+    <h2 style={{textAlign: 'left', width: '90%'}}>1. 오늘 날짜를 선택해주세요.</h2>
       <div style={{display: 'flex', justifyContent: 'center'}}>
         <DatePicker
         selected={selectedDate}
@@ -34,26 +42,35 @@ function Page1({ onMenuClick}) {
         />
       </div>
 
-    <h2 style={{textAlign: 'left', marginTop: '50px'}}>2. 지금 이 설문을 어디에서 하고 계신가요 ?</h2>
-      <div style={{display: 'flex', justifyContent: 'center'}}>
-        <button className="button_y">집</button>
-        <button className="button_y">학교</button>
-        <button className="button_y">카페</button>
-        <button className="button_y">기타</button>
-      </div>
+    <h2 style={{textAlign: 'left', width: '90%'}}>2. 아래 장소 중 학생들이 공부하러 가는 장소를 선택해주세요.</h2>
+      <button className="button" style={{background: 'white', color: 'black', border: '1px solid black'}}>놀이공원</button>
+      <button className="button" style={{background: 'white', color: 'black', border: '1px solid black'}}>학교</button>
+      <button className="button" style={{background: 'white', color: 'black', border: '1px solid black'}}>수영장</button>
+      <button className="button" style={{background: 'white', color: 'black', border: '1px solid black'}}>영화관</button>
+      <button className="button" style={{background: 'white', color: 'black', border: '1px solid black'}}>식당</button>
+     
 
-    <h2 style={{textAlign: 'left', marginTop: '50px'}}>3. 아래 단어를 보고 30초 뒤 기억나는 단어를 모두 선택해주세요.</h2>
-      <div style={{display: 'flex', justifyContent: 'center'}}>
-        <button className="button_y">이 버튼을 누르면 시작됩니다.</button>
+    <h2 style={{textAlign: 'left', width: '90%'}}>3. 아래 단어를 읽고 발음을 올바르게 했는지 알려주세요.</h2>
+      <div style={{display: 'flex', width: '90%', justifyContent: 'space-evenly'}}>
+        <h3 style={{color: 'blue'}}>사과</h3>
+        <h3 style={{color: 'blue'}}>연필</h3>
+        <h3 style={{color: 'blue'}}>시계</h3>
       </div>
+      <div style={{display: 'flex', width: '90%', justifyContent: 'space-evenly'}}>
+        <button className="button" style={{background: 'white', color: 'black', border: '1px solid black'}}>예</button>
+        <button className="button" style={{background: 'white', color: 'black', border: '1px solid black'}}>아니요</button>
+      </div>
+      
 
-    <h2 style={{textAlign: 'left', marginTop: '50px'}}>4. 과일을 가능한 많이 골라주세요.</h2>
-      <div style={{display: 'flex', justifyContent: 'center'}}>
-        <button className="button_y">예</button>
-        <button className="button_n">아니오</button>
-      </div>
+    <h2 style={{textAlign: 'left', width: '90%'}}>4. 100에서 7을 4번 뺀 결과는 얼마입니까?</h2>
+    <input
+    type="number"
+    placeholder="숫자를 입력하세요 예) 34"
+    style={{ width: '80%', padding: '10px', fontSize: '16px' }}
+    />
 
     <button className="button" onClick={() => navigate('/page2')}>다음으로</button>
+    <button className="button" style={{background: 'white', color: 'black', textDecoration: 'underline'}} onClick={() => navigate('/')}>이전으로</button>
     
     </div>
   );
